c38d23c324bdb7d5ae34ba2a2d1230ce404b2c65
Xiang <xiang@silvustechnologies.com>
2015-07-10 13:55:50 -0700

    use args to query DB

--- article/views.py
+++ article/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+#import article
+from article.models import Article
 
 # Create your views here.
 
@@ -7,4 +9,8 @@ def home(request):
     return HttpResponse("Hello world, django")
 
 def detail(request, my_args):
-    return HttpResponse("You're looking at my_args %s. " % my_args)
+    post = Article.objects.all()[int(my_args)]
+    str = ("title = %s, category = %s, date_time = %s, content = %s" % (post.title, post.category, post.date_time, post.content))
+    return HttpResponse(str)
+
+
