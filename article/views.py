3d02370eb24eaa673fb64edb5c77611a62482569
Xiang <xiang@silvustechnologies.com>
2015-07-10 15:27:11 -0700

    Feat: Dynamic URL     - change variable passed name from my_args to id     - post template     - change base and home template     - view from id to query database get content

--- article/views.py
+++ article/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponse
 from article.models import Article
 from datetime import datetime
 
+from django.http import Http404
+
 # Create your views here.
 
 def home(request):
@@ -13,10 +15,15 @@ def home(request):
     }
     return render(request, 'home.html', dic)
 
-def detail(request, my_args):
-    post = Article.objects.all()[int(my_args)]
-    str = ("title = %s, category = %s, date_time = %s, content = %s" % (post.title, post.category, post.date_time, post.content))
-    return HttpResponse(str)
+def detail(request, id):
+    try:
+        post = Article.objects.get(id=str(id))
+    except Article.DoesNotExist:
+        raise Http404
+    dic = {
+        'post': post
+    }
+    return render(request, 'post.html', {'post': post})
 
 def test(request):
     dic = {
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'article.views.home'),
-    url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name='detail'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name='detail'),
     url(r'^test/$', 'article.views.test')
 )
 
